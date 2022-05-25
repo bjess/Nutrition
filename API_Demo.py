9b4e10db726f79f913764d42331cfc90e38ad07c
bjess <16487314+bjess@users.noreply.github.com>
2022-05-24 22:28:17 -0400

    Update API_Demo.py

--- API_Demo.py
+++ API_Demo.py
@@ -1,1 +1,12 @@
+import requests
 
+url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information"
+
+headers = {
+	"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+	"X-RapidAPI-Key": "19553eff50mshef4a13bcaed815ep1e9b9ejsn985a01818a57"
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
